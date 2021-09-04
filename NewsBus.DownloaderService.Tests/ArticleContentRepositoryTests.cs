0d9ca1f2c8510e7a9cacb7c76ea30557ca0918e3
Ruslan Galiev <ruslan.galiev@outlook.com>
2021-09-04 11:16:34 +0300

    Refactored ArticleContentRepositoryTests

--- NewsBus.DownloaderService.Tests/ArticleContentRepositoryTests.cs
+++ NewsBus.DownloaderService.Tests/ArticleContentRepositoryTests.cs
@@ -8,26 +8,36 @@ namespace NewsBus.DownloaderService.Tests
     [TestClass]
     public class ArticleContentRepositoryTests
     {
-        static readonly string storageConnectionString = Environment.GetEnvironmentVariable("NewsBusStorageConnetionString", EnvironmentVariableTarget.Machine);
+        private readonly ArticleContentRepository sut;
+
+        public ArticleContentRepositoryTests()
+        {
+            string storageConnectionString = Environment.GetEnvironmentVariable("NewsBusStorageConnetionString", EnvironmentVariableTarget.Machine);
+            sut = new ArticleContentRepository(storageConnectionString, "integrationtests");
+        }
 
         [TestMethod]
         public async Task PostGetDelete_CreateReadAndDeleteTheBlob_AllSuccessful()
         {
-            ArticleContentRepository target = new ArticleContentRepository(storageConnectionString, "integrationtests");
             string id = Guid.NewGuid().ToString();
             string content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             try
             {
-                bool postResult = await target.PostContentAsync(id, content);
+                // Arrange
+                bool postResult = await sut.PostContentAsync(id, content);
                 Assert.IsTrue(postResult);
-                string actualContent = await target.GetContentAsync(id);
+
+                // Act
+                string actualContent = await sut.GetContentAsync(id);
+
+                // Assert
                 Assert.AreEqual(content, actualContent);
-                bool deleteResult = await target.DeleteContentAsync(id);
-                Assert.IsTrue(deleteResult);
             }
             finally
             {
-                await target.DeleteContentAsync(id);
+                // Clean up
+                bool deleteResult = await sut.DeleteContentAsync(id);
+                Assert.IsTrue(deleteResult);
             }
         }
     }
