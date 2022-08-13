b1ff25abe5774b2e966139ed242c74b78e77c2e1
Mental-NV <ruslan.galiev@outlook.com>
2022-08-13 23:29:09 +0200

    Removed an integration test

--- NewsBus.WatcherService.Tests/RssFeedRepositoryTests.cs
+++ NewsBus.WatcherService.Tests/RssFeedRepositoryTests.cs
@@ -10,22 +10,14 @@ using NewsBus.Infrastructure;
 
 namespace NewsBus.WatcherService.Tests
 {
-    [TestClass]
+    // [TestClass]
     public class RssFeedRepositoryTests
     {
-        [TestMethod]
+        // [TestMethod]
         public async Task GetItems_GetItemsFromDb_GotValidItems()
         {
             // Arrange
             string cosmosConnectionString = Environment.GetEnvironmentVariable("NEWSBUSCOSMOSDBCONNECTIONSTRING", EnvironmentVariableTarget.Process);
-            if (string.IsNullOrWhiteSpace(cosmosConnectionString))
-            {
-                cosmosConnectionString = Environment.GetEnvironmentVariable("NEWSBUSCOSMOSDBCONNECTIONSTRING", EnvironmentVariableTarget.User);
-            }
-            if (string.IsNullOrWhiteSpace(cosmosConnectionString))
-            {
-                cosmosConnectionString = Environment.GetEnvironmentVariable("NEWSBUSCOSMOSDBCONNECTIONSTRING", EnvironmentVariableTarget.Machine);
-            }
             var target = new RssFeedRepository(cosmosConnectionString, Constants.NewsBusDatabase, Constants.RssFeedsContainer);
 
             // Act
